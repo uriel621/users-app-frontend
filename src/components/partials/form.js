c59774518abb927afbb91d426007e4e378bc6667
Ever <uriel621@live.com>
2018-08-22 15:50:58 -0700

    update and create complete

--- src/components/partials/form.js
+++ src/components/partials/form.js
@@ -14,7 +14,7 @@ const Template = (props) => {
             <div className="form-group" hidden>
                 <label className="control-label col-sm-2" htmlFor="email">ID:</label>
                 <div className="col-sm-10">
-                    <input type={ props.type } className="form-control" id="id" name="id" placeholder="id" onChange={ (e) => props.onChangeEvent(e) } value={  props.user.id }  required />
+                    <input type={ props.type } className="form-control" id="id" name="id" placeholder="id" onChange={ (e) => props.onChangeEvent(e) } value={  props.user.id }  />
                 </div>
             </div>
 
@@ -73,13 +73,13 @@ class Form extends Component {
   constructor(props){
     super(props);
     this.state = {
-      "id":"",
-      "birthday":"",
-      "email":"",
-      "favoriteColor":"",
-      "image":"",
-      "name":"",
-      "username":""
+        "id":"",
+        "birthday":"",
+        "email":"",
+        "favoriteColor":"",
+        "image":"",
+        "name":"",
+        "username":""
     }
     // this.onChangeEvent = this.onChangeEvent.bind(this);
     // this.onSubmitEvent = this.onSubmitEvent.bind(this);
@@ -103,24 +103,29 @@ class Form extends Component {
             console.log('create');
         }
         else {
-            // this.props.fetchUser(this.props.match.params.username);
             Axios.get(`http://localhost:4000/edit/${ this.props.match.params.username }`)
-            .then( (response) => {
-                this.setState({ "id":response.data.id })
-                this.setState({ "birthday":response.data.birthday })
-                this.setState({ "email":response.data.email })
-                this.setState({ "favoriteColor":response.data.favoriteColor })
-                this.setState({ "image":response.data.image })
-                this.setState({ "name":response.data.name })
-                this.setState({ "username":response.data.username })
-                // dispatch({
-                //     "type":FETCH_USER,
-                //     "payload":response.data
-                // })
-            })           
-            .catch((error) => {
-                console.log(error);
-            })
+                .then( (response) => {
+                    // console.log("...Loading", response.data);
+                    // for(let key in response.data) {
+                    //     // console.log('state', this.state)
+                    //     console.log('lol', key)
+                    //     console.log('kik', this.state[key]);
+                    //     // this.setState({ key:"l" })
+                    //     // console.log(key)
+                    // }
+                    console.log('LOLOL',response.data)
+                    console.log('LOLOL',response.data.id)
+                    this.setState({ "id":response.data.id })
+                    this.setState({ "birthday":response.data.birthday })
+                    this.setState({ "email":response.data.email })
+                    this.setState({ "favoriteColor":response.data.favoriteColor })
+                    this.setState({ "image":response.data.image })
+                    this.setState({ "name":response.data.name })
+                    this.setState({ "username":response.data.username })
+                })           
+                .catch((error) => {
+                    console.log('Error', error);
+                })
         }
     }
 
@@ -142,89 +147,28 @@ class Form extends Component {
           "name":this.state.name,
           "username":this.state.username
         }
-        this.props.updateUser(post);
+        if(this.props.location.pathname.includes('create')){
+            this.props.createUser(post);
+        }
+        else {
+            this.props.updateUser(post);
+        }
         this.props.history.push('/')
       }
 
   render() {
-    // console.log('R_FORM',this.form);
     console.log('R_STATE', this.state);
-
     if(this.props.location.pathname.includes('create')){
-        // console.log('CREATE');
-        // <Template type={ 'date' }  user={ 'lol' } onChangeEvent={ (e) => this.onChangeEvent(e) } />
+        return(
+            <Template type={ 'date' } onChangeEvent={ (e) => this.onChangeEvent(e) } onSubmitEvent={ (e) => this.onSubmitEvent(e)  } user={ this.state } />
+        )
     }
     else if(this.props.location.pathname.includes('update') && this.state){
+        console.log(621)
         return (
             <Template type={ 'text' } onChangeEvent={ (e) => this.onChangeEvent(e) } onSubmitEvent={ (e) => this.onSubmitEvent(e)  } user={ this.state } />
         )
     }
-    else {
-        return (
-            <div>Uriel</div>
-        )
-    }
-
-    // return (
-    //   <div className="container">
-    //     <div style={{ "textAlign":"center" }}>
-    //       <Link className="nav-link" to="/">
-    //         <button className="btn btn-info" style={{ "width":"50%" }}>Back</button>
-    //       </Link>
-    //     </div>
-    //     <br />
-
-    //     <form className="form-horizontal"  onSubmit={ this.onSubmitEvent } >
-    //       <div className="form-group">
-    //         <label className="control-label col-sm-2" htmlFor="email">Birthday:</label>
-    //         <div className="col-sm-10">
-    //           <input type="date" className="form-control" id="birthday" name="birthday" placeholder="birthday" onChange={ this.onChangeEvent } value={ this.state.birthday } required />
-    //         </div>
-    //       </div>
-
-    //       <div className="form-group">
-    //         <label className="control-label col-sm-2" htmlFor="email">Email:</label>
-    //         <div className="col-sm-10"> 
-    //           <input type="email" className="form-control" id="email" name="email" placeholder="email" onChange={ this.onChangeEvent } value={ this.state.email } required />
-    //         </div>
-    //       </div>
-
-    //       <div className="form-group">
-    //         <label className="control-label col-sm-2" htmlFor="favoriteColor">Favorite Color:</label>
-    //         <div className="col-sm-10"> 
-    //           <input type="text" className="form-control" id="favoriteColor" name="favoriteColor" placeholder="favoriteColor" onChange={ this.onChangeEvent } value={ this.state.favoriteColor } required />
-    //         </div>
-    //       </div>
-
-    //       <div className="form-group">
-    //         <label className="control-label col-sm-2" htmlFor="image">Image URL:</label>
-    //         <div className="col-sm-10"> 
-    //           <input type="text" className="form-control" id="image" name="image" placeholder="image url" onChange={ this.onChangeEvent } value={ this.state.image } required />
-    //         </div>
-    //       </div>
-
-    //       <div className="form-group">
-    //         <label className="control-label col-sm-2" htmlFor="name">Name:</label>
-    //         <div className="col-sm-10"> 
-    //           <input type="text" className="form-control" id="name" name="name" placeholder="name" onChange={ this.onChangeEvent } value={ this.state.name } required />
-    //         </div>
-    //       </div>
-
-    //       <div className="form-group">
-    //         <label className="control-label col-sm-2" htmlFor="username">Username:</label>
-    //         <div className="col-sm-10"> 
-    //           <input type="text" className="form-control" id="username" name="username" placeholder="username" onChange={ this.onChangeEvent } value={ this.state.username } required />
-    //         </div>
-    //       </div>
-
-    //       <div className="form-group"> 
-    //         <div className="col-sm-offset-2 col-sm-10">
-    //           <button type="submit" className="btn btn-default">Submit</button>
-    //         </div>
-    //       </div>
-    //     </form>
-    //   </div>
-    // );
   }
 }
 
